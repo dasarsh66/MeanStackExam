1f5e78258eb545f244ecf3140059be34252e4734
Manikonda <S528807@nwmissouri.edu>
2017-10-11 09:49:05 -0500

    first commit1

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser= require('body-parser')
+const app = express();
+var db
+app.use(bodyParser.urlencoded({extended: true}))  
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  db.collection('EOC').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {EOC: result})
+    console.log(result)
+  })
+})
+
+
+app.get('/i2.ejs', (req, res) => {
+  db.collection('EOC').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('i2.ejs', {EOC: result})
+    console.log(result)
+  })
+})
+
+
+
+
+
+app.post('/quotes', (req, res) => {
+  db.collection('EOC').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/i2.ejs')
+  })
+})
+//var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+// MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+//   if(!err) {
+//     console.log("We are connected");
+//   }
+// });
+//"mongodb://edm:edm@ds111895.mlab.com:11895/eoc"
+const MongoClient = require('mongodb').MongoClient
+MongoClient.connect("mongodb://localhost:27017/exampleDb", (err, database) => {
+  // ... start the server
+if (err) return console.log(err)
+  db = database
+app.listen(8089, function() {
+  console.log('listening ')
+})
+})
+
